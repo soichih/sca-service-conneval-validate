b0f55a85c294650c098c8d036119a26c92e1aa5a
Soichi Hayashi <hayashis@iu.edu>
2018-01-13 21:49:36 -0500

    replaced mrinfo with python nibabel

---
+++ clean.sh
@@ -0,0 +1,1 @@
+rm products.json
--- run.py
+++ run.py
@@ -3,7 +3,7 @@
 import os
 import json
 import re
-import subprocess
+import nibabel
 
 # Things that this script checks
 # 
@@ -30,142 +30,121 @@ with open('config.json') as config_json:
     config = json.load(config_json)
 
 results = {"errors": [], "warnings": []}
-
-#TODO - how should I keep up wit this path?
-mrinfo="/N/soft/rhel6/mrtrix/0.3.15/mrtrix3/release/bin/mrinfo"
-
 directions = None
 
-#parse t1 mrinfo
+def check_affine(affine):
+    if affine[0][1] != 0: results['warnings'].append("affine matrix 0.1 is non 0")
+    if affine[0][2] != 0: results['warnings'].append("affine matrix 0.2 is non 0")
+    if affine[1][0] != 0: results['warnings'].append("affine matrix 1.0 is non 0")
+    if affine[1][2] != 0: results['warnings'].append("affine matrix 1.2 is non 0")
+    if affine[2][0] != 0: results['warnings'].append("affine matrix 2.0 is non 0")
+    if affine[2][1] != 0: results['warnings'].append("affine matrix 2.1 is non 0")
+    if affine[3][0] != 0: results['warnings'].append("affine matrix 3.0 is non 0")
+    if affine[3][1] != 0: results['warnings'].append("affine matrix 3.1 is non 0")
+    if affine[3][2] != 0: results['warnings'].append("affine matrix 3.2 is non 0")
+    if affine[3][3] != 1: results['warnings'].append("affine matrix 3.3 is non 1")
+
 if 't1' in config:
     if config['t1'] is None:
         results['errors'].append("t1 not set")
     else:
+        print("checking t1")
         try: 
-            print "running t1 mrinfo"
-            info = subprocess.check_output([mrinfo, config['t1']], shell=False)
-            results['t1_mrinfo'] = info
-            info_lines = info.split("\n")
-            #print info_lines
+            img = nibabel.load(config['t1'])
+            results['t1_headers'] = str(img.header)
+            results['t1_base_affine'] = str(img.header.get_base_affine())
 
             #check dimentions
-            dim=info_lines[3]
-            dims=dim.split("x")
-            if len(dims) != 3:
-                results['errors'].append("T1 should be 3D but has "+str(len(dims)))
+            if img.header['dim'][0] != 3:
+                results['errors'].append("T1 should be 3D but has "+img.header.dims[0])
 
-            #check transform
-            tl = info_lines[9:12]
-            tl[0] = tl[0][12:] #remove "Transform:"
-            m = []
-            for line in tl:
-                line = line.strip()
-                strs = re.split('\s+', line)
-                ml = []
-                for s in strs:
-                    ml.append(float(s))   
-                m.append(ml)
+            check_affine(img.header.get_base_affine())
 
-            if m[0][0] == 1 and m[0][1] == 0 and m[0][1] == 0 and m[0][0] == 1 and m[0][1] == 0 and m[0][1] == 0 and m[0][0] == 1 and m[0][1] == 0 and m[0][1] == 0:  
-                None #good
-            else:
-                results['warnings'].append("T1 has non-optimal transformation matrix. It should be 1 0 0 / 0 1 0 / 0 0 1")
-            
-        except subprocess.CalledProcessError as err:
-            results['errors'].append("mrinfo failed on t1. error code: "+str(err.returncode))
+        except Exception as e:
+            results['errors'].append(str(e))
 
 if 'dwi' in config:
-
     #check dwi
     if config['dwi'] is None: 
         results['errors'].append("dwi not set")
     else:
+        directions = None
+
+        print("checking dwi")
         try: 
-            print "running dwi mrinfo"
-            info = subprocess.check_output([mrinfo, config['dwi']], shell=False)
-            results['dwi_mrinfo'] = info
-            info_lines = info.split("\n")
+            img = nibabel.load(config['dwi'])
+            results['dwi_headers'] = str(img.header)
+            results['dwi_base_affine'] = str(img.header.get_base_affine())
 
             #check dimentions
-            dim=info_lines[3]
-            dims=dim.split("x")
-            if len(dims) != 4:
-                results['errors'].append("DWI file specified doesn't have 4 dimentions")
-            else:
-                directions = int(dims[3])
-                if directions < 10: 
-                    results['errors'].append("DWI's 4D seems too small",directions)
+            if img.header['dim'][0] != 4:
+                results['errors'].append("DWI should be 4D but has "+img.header.dims[0])
 
-            #check transform
-            tl = info_lines[9:12]
-            tl[0] = tl[0][12:] #remove "Transform:"
-            m = []
-            for line in tl:
-                line = line.strip()
-                strs = re.split('\s+', line)
-                ml = []
-                for s in strs:
-                    ml.append(float(s))   
-                m.append(ml)
+            #check 4d size
+            directions = img.header['dim'][4]
+            if directions < 10:
+                results['warnings'].append("DWI's 4D is too small "+str(directions))
 
-            if m[0][0] == 1 and m[0][1] == 0 and m[0][1] == 0 and m[0][0] == 1 and m[0][1] == 0 and m[0][1] == 0 and m[0][0] == 1 and m[0][1] == 0 and m[0][1] == 0:  
-                None #good
-            else:
-                results['warnings'].append("DWI has non-optimal transformation matrix. It should be 1 0 0 / 0 1 0 / 0 0 1")
-            
-        except subprocess.CalledProcessError as err:
-            results['errors'].append("mrinfo failed on dwi. error code: "+str(err.returncode))
+            #check affine
+            check_affine(img.header.get_base_affine())
 
-    #load bvecs (and check size)
-    if config['bvecs'] is None: 
-        results['errors'].append("bvecs not set")
-    else:
-        try: 
-            bvecs = open(config['bvecs'])
-            bvecs_rows = bvecs.readlines()
-            bvecs_cols = bvecs_rows[0].strip().replace(",", " ")
-	    #remove double spaces
-            bvecs_cols_clean = re.sub(' +', ' ', bvecs_cols) 
-	    bvecs_cols = bvecs_cols_clean.split(' ')		
+        except Exception as e:
+            results['errors'].append(str(e))
 
-            if directions:
+        print("checking bvecs")
+        if config['bvecs'] is None: 
+            results['errors'].append("bvecs not set")
+        else:
+            try: 
+                bvecs = open(config['bvecs'])
+                bvecs_rows = bvecs.readlines()
+                bvecs_cols = bvecs_rows[0].strip().replace(",", " ")
+
+                #remove double spaces
+                bvecs_cols_clean = re.sub(' +', ' ', bvecs_cols) 
+                bvecs_cols = bvecs_cols_clean.split(' ')		
+
+                #bvecs should have same col num as directions
                 if directions != len(bvecs_cols):
                     results['errors'].append("bvecs column count which is "+str(len(bvecs_cols))+" doesn't match dwi's 4d number:"+str(directions))
 
-            if len(bvecs_rows) != 3:
-                results['errors'].append("bvecs should have 3 rows but it has "+str(len(bvecs_rows)))
+                #bvecs should ahve 3 row
+                if len(bvecs_rows) != 3:
+                    results['errors'].append("bvecs should have 3 rows but it has "+str(len(bvecs_rows)))
 
-        except IOError:
-            print "failed to load bvecs:"+config['bvecs']
-            results['errors'].append("Couldn't read bvecs")
+            except IOError:
+                print "failed to load bvecs:"+config['bvecs']
+                results['errors'].append("Couldn't read bvecs")
 
-    #load bvals (and check cols)
-    if config['bvals'] is None: 
-        results['errors'].append("bvals not set")
-    else:
-        try: 
-            bvals = open(config['bvals'])
-            bvals_rows = bvals.readlines()
-            bvals_cols = bvals_rows[0].strip().replace(",", " ")
-	    #remove double spaces
-            bvals_cols_clean = re.sub(' +', ' ', bvals_cols) 
-	    bvals_cols = bvals_cols_clean.split(" ") 
+        print("checking bvals")
+        if config['bvals'] is None: 
+            results['errors'].append("bvals not set")
+        else:
+            try: 
+                bvals = open(config['bvals'])
+                bvals_rows = bvals.readlines()
+                bvals_cols = bvals_rows[0].strip().replace(",", " ")
 
-            if directions:
+                #remove double spaces
+                bvals_cols_clean = re.sub(' +', ' ', bvals_cols) 
+                bvals_cols = bvals_cols_clean.split(" ") 
+
                 if directions != len(bvals_cols):
                     results['errors'].append("bvals column count which is "+str(len(bvals_cols))+" doesn't match dwi's 4d number:"+str(directions))
 
-            if  len(bvals_rows) != 1:
-                results['errors'].append("bvals should have 1 row but it has "+str(len(bvals_rows)))
+                if  len(bvals_rows) != 1:
+                    results['errors'].append("bvals should have 1 row but it has "+str(len(bvals_rows)))
 
-        except IOError:
-            print "failed to load bvals:"+config['bvals']
-            results['errors'].append("Couldn't read bvals")
+            except IOError:
+                print "failed to load bvals:"+config['bvals']
+                results['errors'].append("Couldn't read bvals")
 
-#deprecated
-with open("products.json", "w") as fp:
-    json.dump([{"type": "conneval-validation-result", "results": results}], fp)
+
+#debug..
+print results
 
 with open("product.json", "w") as fp:
     json.dump({"type": "conneval-validation-result", "results": results}, fp)
+
+
 
